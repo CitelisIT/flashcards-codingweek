006c22939a3262758c97a404da29995948dc09f0
NoirayMoussu Camille <camille.noiray-moussu@telecomnancy.eu>
2023-01-03 13:29:19 +0100

    [+]project structure

---
+++ app/src/main/java/flashcards/App.java
@@ -0,0 +1,11 @@
+package flashcards;
+
+public class App {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+}
