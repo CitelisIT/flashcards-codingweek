cbf7b38ae2849145f6142797090a353c245e4f3a
Guillaume RICARD <guillaume.ricard@telecomnancy.eu>
2023-01-04 11:36:52 +0100

    [+] Added deck test

---
+++ app/src/test/java/flashcards/DeckTest.java
@@ -0,0 +1,39 @@
+package flashcards;
+
+import org.junit.jupiter.api.Test;
+
+import flashcards.model.Deck;
+import flashcards.model.Card;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DeckTest {
+
+    @Test
+    void TestDeck() {
+        Deck deck = new Deck();
+        assertEquals("Nouvelle pile", deck.getName());
+        assertEquals("Décrivez votre pile", deck.getDescription());
+        deck.setName("Deck 1");
+        deck.setDescription("Deck 1 description");
+        assertEquals("Deck 1", deck.getName());
+        assertEquals("Deck 1 description", deck.getDescription());
+        Card card = new Card();
+        card.incrApperenceCount();
+        card.incrApperenceCount();
+        card.incrApperenceCount();
+        card.incrRightCount();
+        card.incrRightCount();
+        deck.add(card);
+        assertEquals(2 / 3, deck.getScore());
+        Card card2 = new Card();
+        card2.incrApperenceCount();
+        card2.incrApperenceCount();
+        card2.incrRightCount();
+        deck.add(card2);
+        assertEquals(card2, deck.getCard(1));
+        assertEquals(1 / 2, deck.getScore());
+        deck.remove(0);
+        assertEquals(0, deck.getScore());
+    }
+}
