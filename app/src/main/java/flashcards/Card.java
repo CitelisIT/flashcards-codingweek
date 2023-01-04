898119537a571d76168bd0820ec158ea6c35c76c
Damien <damien.simon@telecomnancy.eu>
2023-01-04 09:11:42 +0100

    [+] add empty class for model

---
+++ app/src/main/java/flashcards/Card.java
@@ -0,0 +1,11 @@
+package flashcards;
+
+import java.util.ArrayList;
+
+public class Card {
+   private ArrayList<Content> question = new ArrayList<Content>();
+   private ArrayList<Content> answer = new ArrayList<Content>();
+   private int apperence_count = 0;
+   private int rhight_count = 0;
+
+}
