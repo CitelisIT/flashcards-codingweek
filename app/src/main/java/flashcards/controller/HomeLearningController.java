62347254c8ca4b0b5a692dd5d59f6831fe94374d
LENFANT Quentin <quentin.lenfant@telecomnancy.eu>
2023-01-05 01:05:07 +0100

    [~] HomeLearning fxml and controller

--- app/src/main/java/flashcards/controller/HomeLearningController.java
+++ app/src/main/java/flashcards/controller/HomeLearningController.java
@@ -1,12 +1,27 @@
 package flashcards.controller;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import flashcards.App;
 import flashcards.model.DeckManager;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 
-public class HomeLearningController implements Observer {
+public class HomeLearningController implements Observer, Initializable{
     private DeckManager allDeck;
+
+    @FXML
+    private ChoiceBox<String> strategyChoiceBox;
+
+    @FXML
+    private Spinner<Integer> nbCardSpinner;
 
     public HomeLearningController(DeckManager allDeck) {
         this.allDeck = allDeck;
@@ -17,8 +32,15 @@ public class HomeLearningController implements Observer {
     }
 
     @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        ObservableList<String> values = FXCollections.observableArrayList("Génération aléatoire","Combler ses lacunes","Valider ses acquis");
+        this.strategyChoiceBox.setItems(values);
+        this.strategyChoiceBox.setValue("Génération aléatoire");
+        this.nbCardSpinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 200, 10));
+    }
+
+    @Override
     public void react() {
         // TODO Auto-generated method stub
-
     }
 }
