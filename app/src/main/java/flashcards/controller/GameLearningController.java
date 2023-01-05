9233d1cb6ad8d4e93361067c0d6a86954bbb6095
LENFANT Quentin <quentin.lenfant@telecomnancy.eu>
2023-01-05 15:03:48 +0100

    [~] gameLearning fxml and controller file

--- app/src/main/java/flashcards/controller/GameLearningController.java
+++ app/src/main/java/flashcards/controller/GameLearningController.java
@@ -1,11 +1,26 @@
 package flashcards.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import flashcards.model.DeckManager;
+import javafx.fxml.Initializable;
 
-public class GameLearningController implements Observer {
+public class GameLearningController implements Observer,Initializable {
 
-    public GameLearningController(DeckManager allDeck, int activeDeck) {
+    private DeckManager flashcardManager;
+    private int activeDeck;
 
+    // TODO Réfléchir à l'implémentation des médias par rapport au timer
+
+    public GameLearningController(DeckManager flashcardManager, int activeDeck) {
+        this.flashcardManager = flashcardManager;
+        this.activeDeck = activeDeck;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
     }
 
     @Override
