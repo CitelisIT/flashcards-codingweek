bff8e88abbbcc454c6d5bc4a2cacd19a67a93ab3
LENFANT Quentin <quentin.lenfant@telecomnancy.eu>
2023-01-06 17:14:35 +0100

    [~] modifications

--- app/src/main/java/flashcards/controller/HomeLearningController.java
+++ app/src/main/java/flashcards/controller/HomeLearningController.java
@@ -43,28 +43,17 @@ public class HomeLearningController implements Observer, Initializable {
     private String currentDeckKey;
     private int currentDeckIndex;
 
-    @FXML
-    private Slider timerSlider;
-    @FXML
-    private ChoiceBox<String> strategyChoiceBox;
-    @FXML
-    private Spinner<Integer> nbCardSpinner;
-    @FXML
-    private VBox rightPannel;
-    @FXML
-    private Accordion accordion;
-    @FXML
-    private Label titleLabel;
-    @FXML
-    private Text descriptionLabel;
-    @FXML
-    private Label strategyLabel;
-    @FXML
-    private Label nbCardLabel;
-    @FXML
-    private Button startButton;
-    @FXML
-    private Label timerLabel;
+    @FXML private Slider timerSlider;
+    @FXML private ChoiceBox<String> strategyChoiceBox;
+    @FXML private Spinner<Integer> nbCardSpinner;
+    @FXML private VBox rightPannel;
+    @FXML private Accordion accordion;
+    @FXML private Label titleLabel;
+    @FXML private Text descriptionLabel;
+    @FXML private Label strategyLabel;
+    @FXML private Label nbCardLabel;
+    @FXML private Button startButton;
+    @FXML private Label timerLabel;
 
     public HomeLearningController(FlashcardManager flashcardManager) {
         this.flashcardManager = flashcardManager;
@@ -144,19 +133,16 @@ public class HomeLearningController implements Observer, Initializable {
         root.requestFocus();
 
         Button questionAnswerButton = (Button) root.lookup("#questionAnswerButton");
-        questionAnswerButton.setStyle(
-                "-fx-pref-width: 490px; -fx-pref-height: 338px;  -fx-font-size: 20px; -fx-font-weight: bold ; -fx-text-alignment: center;");
+        questionAnswerButton.setStyle("-fx-pref-width: 490px; -fx-pref-height: 338px;  -fx-font-size: 20px; -fx-font-weight: bold ; -fx-text-alignment: center;");
         questionAnswerButton.setText(card.getQuestionContent(0).getData());
         questionAnswerButton.setWrapText(true);
         questionAnswerButton.setOnAction(e -> {
             if (questionAnswerButton.getText().equals(card.getQuestionContent(0).getData())) {
                 questionAnswerButton.setText(card.getAnswerContent(0).getData());
-                questionAnswerButton.setStyle(
-                        "-fx-pref-width: 490px; -fx-pref-height: 338px;  -fx-font-size: 16px; -fx-text-alignment: center;");
+                questionAnswerButton.setStyle("-fx-pref-width: 490px; -fx-pref-height: 338px;  -fx-font-size: 16px; -fx-text-alignment: center;");
             } else {
                 questionAnswerButton.setText(card.getQuestionContent(0).getData());
-                questionAnswerButton.setStyle(
-                        "-fx-pref-width: 490px; -fx-pref-height: 338px;  -fx-font-size: 20px; -fx-font-weight: bold ; -fx-text-alignment: center;");
+                questionAnswerButton.setStyle("-fx-pref-width: 490px; -fx-pref-height: 338px;  -fx-font-size: 20px; -fx-font-weight: bold ; -fx-text-alignment: center;");
             }
         });
 
